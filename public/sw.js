25bcadbdd20e44f98381098afbfe9d44eee483c5
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 21:23:30 +0800

    Refresh PWA cache for orange control styling

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "quant-terminal-v5-mobile";
+const CACHE = "quant-terminal-v5-orange-controls";
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(["/", "/manifest.webmanifest", "/icon.svg"]))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
